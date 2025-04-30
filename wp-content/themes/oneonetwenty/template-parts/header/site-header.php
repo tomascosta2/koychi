4677eb1686ed08671bac866f90930df9f4d7436f
tomas <tomascostapp@gmail.com>
2025-04-30 11:54:50 -0300

    Detalle

--- wp-content/themes/oneonetwenty/template-parts/header/site-header.php
+++ wp-content/themes/oneonetwenty/template-parts/header/site-header.php
@@ -27,7 +27,7 @@ $logo_url = wp_get_attachment_image_url($custom_logo_id, 'full');
 					'theme_location' => 'primary',
 					'container' => false,
 					'menu_class' => 'flex flex-col md:flex-row items-center gap-4',
-					'link_before' => '<span class="text-[#060082]">',
+					'link_before' => '<span class="uppercase text-white font-medium">',
 					'link_after' => '</span>',
 					'fallback_cb' => false,
 				));
