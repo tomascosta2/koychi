f23b426052b469772589041322b2397ad2e8c66c
tomas <tomascostapp@gmail.com>
2025-04-30 15:35:38 -0300

    Detalle

--- wp-content/themes/oneonetwenty/template-parts/footer/footer-widgets.php
+++ wp-content/themes/oneonetwenty/template-parts/footer/footer-widgets.php
@@ -11,7 +11,7 @@ if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 
 	<footer class="tcp-footer">
 		<div class="max-w-[1140px] mx-auto px-4 xl:px-0">
-			<div class="tcp-footer__wrapper">
+			<div class="tcp-footer__wrapper grid md:grid-cols-3">
 				<?php dynamic_sidebar( 'sidebar-1' ); ?>
 			</div>
 			<p class="text-[14px] text-white/80 text-center mt-8">© 2025 KOYCHI. Todos los derechos reservados. Sitio Web desarrollado por ABLA Factoría Creativa</p>
