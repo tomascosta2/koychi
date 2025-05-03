47289cc3767d77fa222079277903b7d251003655
tomas <tomascostapp@gmail.com>
2025-05-03 13:54:14 -0300

    Detalle

--- wp-content/themes/oneonetwenty/page-menu.php
+++ wp-content/themes/oneonetwenty/page-menu.php
@@ -155,7 +155,7 @@ get_header(); ?>
 						/>
 					</div>
 				<?php elseif ($index === 9 && !empty($fideos_fields['imagen_entrelinea'])): ?>
-					<div class="my-8 text-center col-span-full">
+					<div class="my-8 text-center">
 						<img 
 							src="<?php echo $fideos_fields['imagen_entrelinea']['url'] ?>" 
 							alt="<?php echo $fideos_fields['imagen_entrelinea']['url'] ?>"
