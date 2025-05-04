fd9e59e7d0af1303cc43146bff18158907500ed9
tomas <tomascostapp@gmail.com>
2025-05-04 12:49:34 -0300

    Sucursales

--- wp-content/themes/oneonetwenty/page-historia.php
+++ wp-content/themes/oneonetwenty/page-historia.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section class="pt-[90px] pb-[100px] bg-[#FF0000]">
+<section class="pt-[70px] md:pt-[90px] pb-[100px] bg-[#FF0000]">
 	<div class="max-w-[1100px] mx-auto px-4">
 		<h1>
 			<img class="mx-auto md:h-[800px] w-auto" src="<?php echo get_field('banner_principal')['url'] ?>" alt="<?php echo get_field('banner_principal')['alt'] ?>">
@@ -25,8 +25,8 @@
 			<?php echo strip_tags(get_field('texto_3')) ?>
 		</p>
 
-		<div class="relative">
-			<img class="mx-auto" src="/wp-content/themes/oneonetwenty/assets/images/koychi-historia-deco-3.png" alt="Deco">
+		<div class="relative mt-8">
+			<img class="mx-auto size-[100px] md:size-[127px]" src="/wp-content/themes/oneonetwenty/assets/images/koychi-historia-deco-3.png" alt="Deco">
 			<h2 class="font-black text-white text-center my-8 text-[40px] md:text-[90px] uppercase leading-[90%]"><?php echo get_field('titulo_4') ?></h2>
 		</div>
 		<p class="text-white text-justify text-[20px] leading-[120%] md:text-[30px] md:leading-[41px]">
