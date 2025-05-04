a9c14507e931ad800f81982c6af2f6305fba5937
tomas <tomascostapp@gmail.com>
2025-05-04 12:21:11 -0300

    Sucursales

--- wp-content/themes/oneonetwenty/page-historia.php
+++ wp-content/themes/oneonetwenty/page-historia.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
-<section class="pt-[110px] pb-[100px] bg-[#FF0000]">
+<section class="pt-[90px] pb-[100px] bg-[#FF0000]">
 	<div class="max-w-[1100px] mx-auto">
-		<img class="mx-auto" src="<?php echo get_field('banner_principal')['url'] ?>" alt="<?php echo get_field('banner_principal')['alt'] ?>">
+		<img class="mx-auto h-[800px] w-auto" src="<?php echo get_field('banner_principal')['url'] ?>" alt="<?php echo get_field('banner_principal')['alt'] ?>">
 		<p class="text-white text-justify text-[30px]">
 			<?php echo strip_tags(get_field('texto_1')) ?>
 		</p>
