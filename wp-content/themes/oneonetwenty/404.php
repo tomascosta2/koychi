a02ba2c221de2907307bf7870ecedd70c7c52780
tomas <tomascostapp@gmail.com>
2025-05-04 12:02:43 -0300

    Sucursales

--- wp-content/themes/oneonetwenty/404.php
+++ wp-content/themes/oneonetwenty/404.php
@@ -2,7 +2,7 @@
 
 <section class="bg-[#111] pt-[200px] pb-[100px]">
 		<div class="tcp-container">
-			<img class="w-[720px] h-[335px] mx-auto object-contain bg-gray-300" src="" alt="">
+			<img class="w-[720px] h-[335px] mx-auto object-contain" src="/wp-content/themes/oneonetwenty/assets/images/404.png" alt="">
 			<p class="text-white text-center font-semibold text-[30px] mt-8">Esta página no existe, ¡pero nuestro menú sí!</p>
 		</div>
 </section>
