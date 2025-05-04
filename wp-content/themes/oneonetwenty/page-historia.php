be7c5e55f8149bb4b97698427e70d1f7bddff560
tomas <tomascostapp@gmail.com>
2025-05-04 12:31:12 -0300

    Sucursales

--- wp-content/themes/oneonetwenty/page-historia.php
+++ wp-content/themes/oneonetwenty/page-historia.php
@@ -1,26 +1,26 @@
 <?php get_header(); ?>
 
 <section class="pt-[90px] pb-[100px] bg-[#FF0000]">
-	<div class="max-w-[1100px] mx-auto">
+	<div class="max-w-[1100px] mx-auto px-4">
 		<h1>
 			<img class="mx-auto h-[800px] w-auto" src="<?php echo get_field('banner_principal')['url'] ?>" alt="<?php echo get_field('banner_principal')['alt'] ?>">
 		</h1>
-		<p class="text-white text-justify text-[20px] leading-[41px]">
+		<p class="text-white text-justify text-[20px] leading-[110%] md:leading-[41px]">
 			<?php echo strip_tags(get_field('texto_1')) ?>
 		</p>
 
 		<h2 class="font-black text-white text-center my-8 text-[40px] md:text-[90px] uppercase leading-[90%]"><?php echo get_field('titulo_2') ?></h2>
-		<p class="text-white text-justify text-[20px] leading-[41px]">
+		<p class="text-white text-justify text-[20px] leading-[110%] md:leading-[41px]">
 			<?php echo strip_tags(get_field('texto_2')) ?>
 		</p>
 
 		<h2 class="font-black text-white text-center my-8 text-[40px] md:text-[90px] uppercase leading-[90%]"><?php echo get_field('titulo_3') ?></h2>
-		<p class="text-white text-justify text-[20px] leading-[41px]">
+		<p class="text-white text-justify text-[20px] leading-[110%] md:leading-[41px]">
 			<?php echo strip_tags(get_field('texto_3')) ?>
 		</p>
 
 		<h2 class="font-black text-white text-center my-8 text-[40px] md:text-[90px] uppercase leading-[90%]"><?php echo get_field('titulo_4') ?></h2>
-		<p class="text-white text-justify text-[20px] leading-[41px]">
+		<p class="text-white text-justify text-[20px] leading-[110%] md:leading-[41px]">
 			<?php echo strip_tags(get_field('texto_4')) ?>
 		</p>
 		<img class="mx-auto mt-8" src="<?php echo get_field('banner_final')['url'] ?>" alt="<?php echo get_field('banner_final')['alt'] ?>">
