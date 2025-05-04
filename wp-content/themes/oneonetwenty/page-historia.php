62576d61f2cc69f63cbcc0a243fac08535089e12
tomas <tomascostapp@gmail.com>
2025-05-04 12:22:58 -0300

    Sucursales

--- wp-content/themes/oneonetwenty/page-historia.php
+++ wp-content/themes/oneonetwenty/page-historia.php
@@ -2,9 +2,21 @@
 
 <section class="pt-[90px] pb-[100px] bg-[#FF0000]">
 	<div class="max-w-[1100px] mx-auto">
-		<img class="mx-auto h-[800px] w-auto" src="<?php echo get_field('banner_principal')['url'] ?>" alt="<?php echo get_field('banner_principal')['alt'] ?>">
-		<p class="text-white text-justify text-[30px]">
+		<h1>
+			<img class="mx-auto h-[800px] w-auto" src="<?php echo get_field('banner_principal')['url'] ?>" alt="<?php echo get_field('banner_principal')['alt'] ?>">
+		</h1>
+		<p class="text-white text-justify text-[30px] leading-[41px]">
 			<?php echo strip_tags(get_field('texto_1')) ?>
+		</p>
+
+		<h2><?php echo get_field('titulo_2') ?></h2>
+		<p class="text-white text-justify text-[30px] leading-[41px]">
+			<?php echo strip_tags(get_field('texto_2')) ?>
+		</p>
+
+		<h2><?php echo get_field('titulo_3') ?></h2>
+		<p class="text-white text-justify text-[30px] leading-[41px]">
+			<?php echo strip_tags(get_field('texto_3')) ?>
 		</p>
 	</div>
 </section>
