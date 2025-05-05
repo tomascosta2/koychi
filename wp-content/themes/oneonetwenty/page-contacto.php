da4b7e35c2ab3602d7c071574879201308e61616
tomas <tomascostapp@gmail.com>
2025-05-05 10:49:12 -0300

    Detalle

--- wp-content/themes/oneonetwenty/page-contacto.php
+++ wp-content/themes/oneonetwenty/page-contacto.php
@@ -5,7 +5,9 @@
 		<p class="text-[48px] leading-[100%] font-medium text-white text-center"><?php echo strip_tags(get_field('span')); ?></p>
 		<h1 class="text-[96px] leading-[100%] font-black text-white text-center"><?php echo get_field('titulo'); ?></h1>
 		<div class="grid grid-cols-2 gap-4">
-			<img src="<?php echo get_field('imagen')['url']; ?>" alt="<?php echo get_field('imagen')['alt']; ?>">
+			<div class="flex items-baseline">
+				<img src="<?php echo get_field('imagen')['url']; ?>" alt="<?php echo get_field('imagen')['alt']; ?>">
+			</div>
 			<div>
 				<?php echo do_shortcode('[contact-form-7 id="cba1be0" title="Contact form 1"]') ?>
 			</div>
