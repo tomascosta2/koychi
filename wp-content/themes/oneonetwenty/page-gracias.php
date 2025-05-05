3a672f288ca7fe5d3eb760f4ca57970e239a0235
tomas <tomascostapp@gmail.com>
2025-05-05 11:17:49 -0300

    Detalle

--- wp-content/themes/oneonetwenty/page-gracias.php
+++ wp-content/themes/oneonetwenty/page-gracias.php
@@ -3,11 +3,11 @@
 <section class="bg-[#FF0000] pt-[90px]">
 	<div class="tcp-container">
 		<div class="flex">
-			<div class="relative md:min-w-[580px]">
-				<img class="bg-gray-300 w-[580px] h-full absolute -right-[40px] top-0" src="<?php echo get_field('imagen_1')['url'] ?>" alt="<?php echo get_field('imagen_1')['alt'] ?>">
+			<div class="relative md:min-w-[550px]">
+				<img class="bg-gray-300 w-[550px] h-full absolute -right-[40px] top-0" src="<?php echo get_field('imagen_1')['url'] ?>" alt="<?php echo get_field('imagen_1')['alt'] ?>">
 			</div>
 			<div class="pb-[60px] pt-[30px] relative">
-				<h1 class="font-black text-white text-[100px] leading-[90%]">
+				<h1 class="font-black text-white text-[95px] leading-[90%]">
 					<?php echo get_field('titulo') ?>
 				</h1>
 				<p class="font-black text-white text-[50px] pl-[64px] leading-[100%]">
