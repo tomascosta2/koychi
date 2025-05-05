5cba647894fda446767e6f31b11b9d0f98b97085
tomas <tomascostapp@gmail.com>
2025-05-05 11:12:01 -0300

    Detalle

--- wp-content/themes/oneonetwenty/page-gracias.php
+++ wp-content/themes/oneonetwenty/page-gracias.php
@@ -1,19 +1,19 @@
 <?php get_header() ?>
 
-<section class="bg-[#FF0000]">
+<section class="bg-[#FF0000] pt-[90px]">
 	<div class="tcp-container">
 		<div class="flex">
 			<div>
 				<img class="bg-gray-300 w-[615px] h-full" src="<?php echo get_field('imagen_1')['url'] ?>" alt="<?php echo get_field('imagen_1')['alt'] ?>">
 			</div>
-			<div>
-				<h1>
+			<div class="pb-[60px] pt-[30px]">
+				<h1 class="font-black text-white text-[100px] leading-[90%]">
 					<?php echo get_field('titulo') ?>
 				</h1>
-				<p>
+				<p class="font-black text-white text-[50px] pl-[64px]">
 					<?php echo strip_tags(get_field('bajada')) ?>
 				</p>
-				<img src="<?php echo get_field('imagen_2')['url'] ?>" alt="<?php echo get_field('imagen_2')['alt'] ?>">
+				<img class="pl-[120px]" src="<?php echo get_field('imagen_2')['url'] ?>" alt="<?php echo get_field('imagen_2')['alt'] ?>">
 			</div>
 		</div>
 	</div>
