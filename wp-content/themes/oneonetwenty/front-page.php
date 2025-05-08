fe55d8ddbbce55f840ec6cd35a89f655bbac0153
tomas <tomascostapp@gmail.com>
2025-05-08 11:55:24 -0300

    Detalle

--- wp-content/themes/oneonetwenty/front-page.php
+++ wp-content/themes/oneonetwenty/front-page.php
@@ -15,13 +15,13 @@ get_header(); ?>
 		<div class="swiper-wrapper max-h-full">
 			<!-- Slide 1 -->
 			<?php foreach ($hero_fields['slides'] as $slide) : ?>
-				<div class="swiper-slide bg-cover bg-[url('<?php echo $slide['imagen']['url'] ?>')] pt-[90px] pb-[66px] flex items-center justify-center bg-gray-500">
+				<div class="swiper-slide group bg-cover bg-[url('<?php echo $slide['imagen']['url'] ?>')] pt-[90px] pb-[66px] flex items-center justify-center bg-gray-500">
 					<div class="tcp-container h-full">
 						<div class="flex items-end justify-center h-full">
 							<div class="bg-white py-4 px-8 rounded-[15px] h-fit relative">
-								<img class="hidden md:block absolute -left-[40px] -bottom-[25px] -rotate-25"
+								<img class="hidden group-hover:-rotate-35 md:block absolute -left-[40px] -bottom-[25px] -rotate-25"
 									src="/wp-content/themes/oneonetwenty/assets/images/koychi-element-1.png" alt="Koychi K">
-								<img class="absolute size-[50px] md:size-[115px] -top-[20px] md:-right-[57.5px] -right-[10px] md:-top-[57.5px]"
+								<img class="absolute group-hover:rotate-10 size-[50px] md:size-[115px] -top-[20px] md:-right-[57.5px] -right-[10px] md:-top-[57.5px]"
 									src="/wp-content/themes/oneonetwenty/assets/images/koychi-element-2.png" alt="Koychi K">
 								<h2 class="ky-hero__title font-bold uppercase text-[32px] md:text-[60px] leading-[79%] text-center">
 									<?php echo $slide['titulo'] ?>
