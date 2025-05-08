fcb9ba28114b68a7691de33194810b0cd7e5fc8c
tomas <tomascostapp@gmail.com>
2025-05-08 12:04:26 -0300

    Detalle

--- wp-content/themes/oneonetwenty/front-page.php
+++ wp-content/themes/oneonetwenty/front-page.php
@@ -48,10 +48,10 @@ get_header(); ?>
 <section class="ky-banners">
 	<div class="grid md:grid-cols-2">
 		<a class="overflow-clip md:rounded-br-[20px] md:rounded-bl-[20px]" href="<?php echo $banners_fields['enlace_banner_1'] ?>">
-			<img class="md:h-[280px] hover:scale-[105%] h-auto w-full bg-gray-300 object-cover md:rounded-br-[20px] md:rounded-bl-[20px]" src="<?php echo $banners_fields['imagen_banner_1']['url'] ?>" alt="">
+			<img class="md:h-[280px] hover:scale-[105%] transition-all h-auto w-full bg-gray-300 object-cover md:rounded-br-[20px] md:rounded-bl-[20px]" src="<?php echo $banners_fields['imagen_banner_1']['url'] ?>" alt="">
 		</a>
 		<a class="overflow-clip md:rounded-br-[20px] md:rounded-bl-[20px]" href="<?php echo $banners_fields['enlace_banner_2'] ?>">
-			<img class="md:h-[280px] hover:scale-[105%] h-auto w-full bg-gray-300 object-cover md:rounded-br-[20px] md:rounded-bl-[20px]" src="<?php echo $banners_fields['imagen_banner_2']['url'] ?>" alt="">
+			<img class="md:h-[280px] hover:scale-[105%] transition-all h-auto w-full bg-gray-300 object-cover md:rounded-br-[20px] md:rounded-bl-[20px]" src="<?php echo $banners_fields['imagen_banner_2']['url'] ?>" alt="">
 		</a>
 	</div>
 </section>
