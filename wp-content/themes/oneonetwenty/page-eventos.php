4dda9904d209a18847a3e600bda0e3cd68b3df70
tomas <tomascostapp@gmail.com>
2025-05-27 19:48:44 -0300

    Eventos

---
+++ wp-content/themes/oneonetwenty/page-eventos.php
@@ -0,0 +1,131 @@
+<?php get_header(); ?>
+
+<!-- Hero -->
+<section class="relative min-h-[740px]">
+	<div class="bg-cover bg-[url('/wp-content/themes/oneonetwenty/assets/images/eventos-fondo-hero.jpg')] w-full h-[740px] absolute top-0 left-0"></div>
+	<div class="relative z-40s pt-[150px]">
+		<h1 class="text-center max-w-[800px] mx-auto uppercase leading-[80%] text-[170px] font-black text-white text-[]">
+			más
+			cerca de
+			<span class="text-transparent">.</span>  ti.
+		</h1>
+		<img class="-mt-[180px] mx-auto max-w-[1300px] pe-[100px]" src="/wp-content/themes/oneonetwenty/assets/images/eventos-carrito-hero.png" alt="Koychi Eventos Carrito">
+	</div>
+</section>
+
+<!-- Eventos -->
+<section>
+	<div class="tcp-container">
+		<h2 class="uppercase text-center font-black text-[96px] leading-[106px]">En KOYCHI eventos</h2>
+		<p class="py-4 text-[24px] leading-[100%] md:text-[32px] md:leading-[34px] max-w-[940px] mx-auto text-black px-8 md:px-[100px] bg-[#FEC600]">
+			Transformamos cada celebración en una experiencia única, con propuestas creativas que combinan sabor, estilo y personalidad.
+		</p>
+		<div class="bg-black max-w-[940px] mx-auto mt-[57px] p-8 md:px-[90px]">
+			<h3 class="text-white text-center font-black uppercase text-[48px] leading-[64px] mb-8">
+				Nos adaptamos a cada ocasión
+				para hacerla inolvidable
+			</h3>
+			<p class="text-[32px] leading-[40px] text-white">
+				Desde eventos masivos con nuestro foodtruck hasta celebraciones privadas y encuentros corporativos con nuestro servicio de catering.
+			</p>
+		</div>
+	</div>
+	<img class="w-full bg-green-500 h-[1030px]" src="" alt="Koychi Eventos">
+</section>
+
+<!-- Creemos que... -->
+<section class="py-[80px]"> 
+	<div class="relative">
+		<img class="absolute -top-[60px] left-[calc(50%-620px)] max-w-[1240px] mx-auto" src="/wp-content/themes/oneonetwenty/assets/images/eventos-creemos-fondo.png" alt="Fondo">
+		<h2 class="text-center pt-6 pb-10 bg-no-repeat bg-[url('/wp-content/themes/oneonetwenty/assets/images/papel-fondo-titulo-creemos.png')] font-black -rotate-2 uppercase max-w-[880px] mx-auto text-[110px] leading-[113px]">
+			En KOYCHI
+			creemos que
+		</h2>
+	</div>
+	<div class="relative z-50 max-w-[860px] -rotate-[1.6deg] mx-auto py-4 bg-black">
+		<h3 class="text-[#FF0000] font-bold text-center text-[48px] leading-[40px]">La comida es cultura viva:</h3>
+		<p class="text-white text-center text-[32px] leading-[40px]">Compartimos tradiciones coreanas a través de sabores auténticos y llenos de identidad.</p>
+	</div>
+	<div class="relative z-50 max-w-[860px] -rotate-[0.24deg] mx-auto py-4 bg-black my-8">
+		<h3 class="text-[#FF0000] font-bold text-center text-[48px] leading-[40px]">Cada experiencia debe emocionar:</h3>
+		<p class="text-white text-center text-[32px] leading-[40px]">Desde un almuerzo en nuestras sucursales hasta un gran evento empresarial.</p>
+	</div>
+	<div class="relative z-50 max-w-[860px] -rotate-[1.6deg] mx-auto py-4 bg-black">
+		<h3 class="text-[#FF0000] font-bold text-center text-[48px] leading-[40px]">La innovación y la autenticidad no son opuestos:</h3>
+		<p class="text-white text-center text-[32px] leading-[40px]">Usamos ingredientes de calidad para crear propuestas únicas sin perder la esencia tradicional.</p>
+	</div>
+</section>
+
+<!-- Para quien es -->
+<section>
+	<div class="tcp-container">
+		<h2 class="text-center pt-6 pb-10 bg-no-repeat bg-contain bg-[url('/wp-content/themes/oneonetwenty/assets/images/papel-fondo-titulo-paraquien.png')] font-black rotate-2 uppercase max-w-[1030px] mx-auto text-white text-[110px] leading-[80%]">
+			Nuestra propuesta está dirigida a:
+		</h2>
+		<div class="grid grid-cols-3 gap-x-[40px] gap-y-[30px]">
+			<div class="bg-[#FF0000] p-8 rounded-[20px]">
+				<img class="h-[60px] w-auto mx-auto bg-gray-200" src="" alt="">
+				<p class="text-white text-center text-[32px] leading-[29px] mt-4">
+					Empresas que buscan eventos corporativos únicos.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Pepe -->
+<section>
+	<div class="tcp-container">
+		<img 
+			class="w-[1040px] relative z-40 h-[690px] mx-auto -rotate-[3.5deg] bg-green-500 border border-[17px] border-white" 
+			src="" 
+			alt=""
+		/>
+		<img 
+			class="w-[1040px] relative z-10 h-[690px] mx-auto rotate-[3.12deg] bg-green-500 border border-[17px] border-white" 
+			src="" 
+			alt=""
+		/>
+		<img class="-mt-[180px] relative z-40 mx-auto max-w-[700px] pe-[100px]" src="/wp-content/themes/oneonetwenty/assets/images/eventos-carrito-hero.png" alt="Koychi Eventos Carrito">
+		<div class="max-w-[1000px] mx-auto -mt-8">
+			<h2 class="text-[100px] text-center leading-[107px] uppercase font-bold">¿Quieres tener a Koychi en tu evento o celebración?</h2>
+			<a class="text-[48px] leading-[69%] mt-8 bg-[#FF0000] py-[22px] px-8 rounded-full text-white uppercase block w-fit mx-auto font-bold" href="">¡Contáctanos!</a>
+		</div>
+	</div>
+</section>
+
+<!-- Gallery -->
+<section class="py-[100px]">
+	<div class="tcp-container">
+		<div class="grid grid-cols-3 gap-[20px]">
+			<div class="bg-green-500 col-span-2 rounded-[20px] h-[343px]">
+
+			</div>
+			<div class="bg-green-500 rounded-[20px] h-[343px]">
+
+			</div>
+			<div class="bg-green-500 rounded-[20px] h-[343px]">
+
+			</div>
+			<div class="bg-green-500 col-span-2 rounded-[20px] h-[343px]">
+
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="py-[80px] bg-cover bg-[url('/wp-content/themes/oneonetwenty/assets/images/eventos-fondo-contacto.jpg')]">
+	<div class="tcp-container">
+		<h2 class="max-w-[1100px] text-center mx-auto font-black text-[80px] text-white leading-[73px]">
+			¡Contáctanos para tener a Koychi en tu evento o celebración!
+		</h2>
+		<div class="grid grid-cols-2">
+			<img class="w-[644px] h-[577px] bg-green-500" src="" alt="">
+			<div class="md:order-none -order-1">
+				<?php echo do_shortcode('[contact-form-7 id="cba1be0" title="Contact form 1"]') ?>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
